cdd797b6ddff0ab514e2f29061b7c1247b7586a3
Dannyfsp <femoscopilala@gmail.com>
2023-05-09 16:29:05 -0700

    MongoDB operations with python using pymongo

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -3,6 +3,8 @@
 MongoDB Operations with Python using pymongo
 '''
 
+from pymongo import MongoClient
+
 
 if __name__ == "__main__":
     ''' Provides some stats about Nginx logs stored in MongoDB '''
