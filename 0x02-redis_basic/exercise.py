214fea5c6987b4c4a2d0cab16d1bce364139ce84
Dannyfsp <femoscopilala@gmail.com>
2023-05-10 20:27:02 +0000

    Decorator to store the history of inputs and outputs

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,6 +25,50 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    '''
+    Decorator to store the history of inputs and outputs for
+    a particular function
+    '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        ''' Wrapper for decorator functionality '''
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ':inputs', input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ':outputs', output)
+        return output
+
+    return wrapper
+
+
+def replay(fn: Callable):
+    ''' Display the history calls of a particular function'''
+    r = redis.Redis()
+    f_name = fn.__qualname__
+    n_calls = r.get(f_name)
+    try:
+        n_calls = n_calls.decode('utf-8')
+    except Exception:
+        n_calls = 0
+    print(f'{f_name} was called {n_calls} times:')
+
+    ins = r.lrange(f_name + ':inputs', 0, -1)
+    outs = r.lrange(f_name + ':outputs', 0, -1)
+
+    for i, o in zip(ins, outs):
+        try:
+            i = i.decode('utf-8')
+        except Exception:
+            i = ''
+        try:
+            o = o.decode('utf-8')
+        except Exception:
+            o = ''
+
+        print(f'{f_name}(*{i}) -> {o}')
+
+
 class Cache:
     ''' Class for implementing a Cache with redis '''
 
@@ -33,6 +77,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
